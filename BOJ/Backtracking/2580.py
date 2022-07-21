4ae79c8609caf3378c4498dad3223b2ff355c343
s2lee <therok.manager@gmail.com>
2022-07-21 20:07:46 +0900

    Add 2580 : Backtracking

---
+++ BOJ/Backtracking/2580.py
@@ -0,0 +1,36 @@
+import sys
+
+
+board = [list(map(int, input().split())) for _ in range(9)]
+zeros = [(i, j) for i in range(9) for j in range(9) if board[i][j] == 0]
+
+
+def get_possibles(r, c):
+    possibles = set(range(1, 10))
+    possibles -= set(board[r])
+    tmp = set()
+    for i in range(9):
+        tmp.add(board[i][c])
+
+    tmp = set()
+    for i in range(r//3 * 3, r//3 * 3 + 3):
+        for j in range(c//3 * 3, c//3 * 3 + 3):
+            tmp.add(board[i][j])
+    possibles -= tmp
+
+    return possibles
+
+
+def solve(i):
+    if i == len(zeros):
+        [print(*row) for row in board]
+        sys.exit()
+    r, c = zeros[i]
+    possibles = get_possibles(r, c)
+    for num in possibles:
+        board[r][c] = num
+        solve(i + 1)
+        board[r][c] = 0
+
+
+solve(0)
