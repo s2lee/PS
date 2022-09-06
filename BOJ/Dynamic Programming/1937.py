7be240112d24809ddd03f4f48a8834c33800ba22
s2lee <therok.manager@gmail.com>
2022-09-07 07:15:54 +0900

    Add 1937 : Dynamic Programming

---
+++ BOJ/Dynamic Programming/1937.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(100000)
+
+
+def dfs(x, y):
+    if dp[x][y] != 0:
+        return dp[x][y]
+    dp[x][y] = 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+            if graph[x][y] < graph[nx][ny]:
+                dp[nx][ny] = max(dp[x][y], dfs(nx, ny) + 1)
+    return dp[x][y]
+
+
+n = int(input())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+result = 0
+for i in range(n):
+    for j in range(n):
+        result = max(result, dfs(i, j))
+
+print(result)
