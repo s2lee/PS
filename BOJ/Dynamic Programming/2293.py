81ae8869ae41784a813f460f8255d840f3f48f27
s2lee <therok.manager@gmail.com>
2022-09-26 21:01:31 +0900

    Add 2293 : Dynamic Programming

---
+++ BOJ/Dynamic Programming/2293.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+coin = [int(input()) for _ in range(n)]
+
+dp = [0 for _ in range(k + 1)]
+dp[0] = 1
+for i in coin:
+    for j in range(1, k + 1):
+        if j - i >= 0:
+            dp[j] += dp[j - i]
+
+print(dp[k])
