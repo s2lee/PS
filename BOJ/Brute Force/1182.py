ef214d269345caf474067e0e9f31b6e0064d5d00
s2lee <therok.manager@gmail.com>
2022-09-30 22:05:39 +0900

    Add 1182 : Brute Force

---
+++ BOJ/Brute Force/1182.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+
+n, s = map(int, input().split())
+data = list(map(int, input().split()))
+
+count = 0
+for i in range(1, n + 1):
+    for x in combinations(data, i):
+        if sum(x) == s:
+            count += 1
+
+print(count)
