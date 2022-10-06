44be7b94fe15f5e311b3062069ed04d61c8114e8
s2lee <therok.manager@gmail.com>
2022-10-06 20:23:32 +0900

    Add 2470 : Binary Search

---
+++ BOJ/Binary Search/2470.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+array = sorted(list(map(int, input().rsplit())))
+left, right = 0, n - 1
+lowest = sys.maxsize
+low_l, low_r = 0, 0
+while left < right:
+    sum_value = array[left] + array[right]
+    if abs(sum_value) < lowest:
+        lowest = abs(sum_value)
+        low_l, low_r = array[left], array[right]
+
+    if sum_value >= 0:
+        right -= 1
+    else:
+        left += 1
+
+print(low_l, low_r)
