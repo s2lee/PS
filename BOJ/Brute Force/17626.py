2c15054fb1b51e293d39480132eefcd1c27cdbd8
s2lee <therok.manager@gmail.com>
2022-11-10 08:16:41 +0900

    Add 17626 : Brute Force

---
+++ BOJ/Brute Force/17626.py
@@ -0,0 +1,14 @@
+def solve(n):
+    if (n ** 0.5).is_integer():
+        return 1
+    for i in range(1, int(n ** 0.5) + 1):
+        if ((n - i * i) ** 0.5).is_integer():
+            return 2
+    for i in range(1, int(n ** 0.5) + 1):
+        for j in range(1, int((n - i * i) ** 0.5) + 1):
+            if ((n - i * i - j * j) ** 0.5).is_integer():
+                return 3
+    return 4
+
+
+print(solve(int(input())))
